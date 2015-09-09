ae4379702742601cb261dd6d2033a981014b60ec
Tony Bowden <tony@tmtm.com>
2015-09-09 08:27:37 +0100

    initial scraper

---
+++ scraper.rb
@@ -0,0 +1,31 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'json'
+require 'pry'
+require 'rest-client'
+require 'scraperwiki'
+require 'wikidata/fetcher'
+require 'mediawiki_api'
+
+def members
+  morph_api_url = 'https://api.morph.io/tmtmtmtm/us-congress-members/data.json'
+  morph_api_key = ENV["MORPH_API_KEY"]
+  result = RestClient.get morph_api_url, params: {
+    key: morph_api_key,
+    query: "select DISTINCT(identifier__wikipedia) AS wikiname from data WHERE house = 'sen'"
+  }
+  JSON.parse(result, symbolize_names: true)
+end
+
+WikiData.ids_from_pages('en', members.map { |c| c[:wikiname] }).each_with_index do |p, i|
+  puts i if (i % 20).zero?
+  data = WikiData::Fetcher.new(id: p.last).data rescue nil
+  unless data
+    warn "No data for #{p}"
+    next
+  end
+  puts data
+  ScraperWiki.save_sqlite([:id], data)
+end
+
