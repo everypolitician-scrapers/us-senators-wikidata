7c68db10c6947556b1eb856076276d5794b7f389
Tony Bowden <tony@tmtm.com>
2016-02-06 11:12:53 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -18,16 +18,5 @@ def members
   JSON.parse(result, symbolize_names: true)
 end
 
-WikiData.ids_from_pages('en', members.map { |c| c[:wikiname] }).each_with_index do |p, i|
-  puts i if (i % 20).zero?
-  data = WikiData::Fetcher.new(id: p.last).data rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  data[:original_wikiname] = p.first
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
-
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: members.map { |w| w[:wikiname] } })
+warn EveryPolitician::Wikidata.notify_rebuilder
