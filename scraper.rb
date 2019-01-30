a3dcfd89492b82a74c8f5c268123d99708fe0afa
Tony Bowden <tony@tmtm.com>
2019-01-30 09:07:12 +0000

    Deal in Wikidata IDs throughout
    Everyone in enwiki now has suitable P39s, so we no longer need this
    lookup.

--- scraper.rb
+++ scraper.rb
@@ -8,26 +8,17 @@ require 'scraperwiki'
 require 'wikidata/fetcher'
 require 'mediawiki_api'
 
+# TODO remove this once all members are have suitable P39s
 def members
   morph_api_url = 'https://api.morph.io/tmtmtmtm/us-congress-members/data.json'
   morph_api_key = ENV["MORPH_API_KEY"]
   result = RestClient.get morph_api_url, params: {
     key: morph_api_key,
-    query: "select DISTINCT(identifier__wikipedia) AS wikiname from data WHERE house = 'sen'"
+    query: "select DISTINCT(identifier__wikidata) AS wikidata from data WHERE house = 'sen'"
   }
   JSON.parse(result, symbolize_names: true)
 end
-
-morph_names = members.map { |w| w[:wikiname] }
-
-url = "https://en.wikipedia.org/wiki/115th_United_States_Congress"
-wp_names = EveryPolitician::Wikidata.wikipedia_xpath(
-  url: url,
-  after: '//span[@id="Members"]',
-  before: '//span[@id="House_of_Representatives_3"]',
-  xpath: './/li//a[not(@class="new")]/@title',
-)
-raise "No names at #{url}" if wp_names.empty?
+morph = members.map { |w| w[:wikidata] }
 
 sparq = <<EOQ
   SELECT DISTINCT ?item ?start ?end WHERE {
@@ -40,4 +31,4 @@ sparq = <<EOQ
 EOQ
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: morph_names | wp_names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids | morph)
