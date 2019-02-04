649d62a9858fa9a7a23d75f2fa8d0fe1c6bb6d5c
Tony Bowden <tony@tmtm.com>
2019-02-04 11:14:20 +0000

    Find relevant members only via Wikiata
    Everyone should now have a suitable P39, so we don't need to combine the
    two sources.

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,7 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'json'
-require 'pry'
-require 'rest-client'
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'mediawiki_api'
-
-# TODO remove this once all members are have suitable P39s
-def members
-  morph_api_url = 'https://api.morph.io/tmtmtmtm/us-congress-members/data.json'
-  morph_api_key = ENV["MORPH_API_KEY"]
-  result = RestClient.get morph_api_url, params: {
-    key: morph_api_key,
-    query: "select DISTINCT(identifier__wikidata) AS wikidata from data WHERE house = 'sen'"
-  }
-  JSON.parse(result, symbolize_names: true)
-end
-morph = members.map { |w| w[:wikidata] }
 
 sparq = <<EOQ
   SELECT DISTINCT ?item ?start ?end WHERE {
@@ -31,4 +14,4 @@ sparq = <<EOQ
 EOQ
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids | morph, batch_size: 200)
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, batch_size: 200)
